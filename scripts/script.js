aec8c5d0c04c01e657db480a2afb8a9857e1f3e3
Maxim Mishustin <vs1.136@yandex.ru>
2023-02-18 18:32:31 +0300

    отправить на ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,28 @@
-// Находим форму в DOM
-const editProfileButton = document.querySelector('.profile__edit-button');
-editProfileButton.addEventListener('click', function () {
-    const editPopup = document.querySelector('.popup');
-    console.log(editPopup);
-    editPopup.classList.add('popup_opened'); // почему класс попап с кобках без точки?
-}); 
- 
-const editPopupCloseButton = document.querySelector('.popup');
-editPopupCloseButton.addEventListener('click', function() {
-    const editPopup = document.querySelector('.popup');
-    console.log(editPopup); 
-    editPopup.classList.add('popup_opened');
-});
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupButtonClose = document.querySelector('.popup__button-close');
+const profileTitle = document.querySelector('.profile__title');
+const popupInputTypeName = document.querySelector('.popup__input_type_name');
+const profileText = document.querySelector('.profile__text');
+const popupInputTypeJob = document.querySelector('.popup__input_type_job');
+const popupForm = document.querySelector('.popup__form');
 
-
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__input_type_job');// Воспользуйтесь инструментом .querySelector()
-let jobInput = // Воспользуйтесь инструментом .querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+function formSave (evt) {
+    evt.preventDefault(); 
+    profileTitle.textContent = popupInputTypeName.value;
+    profileText.textContent = popupInputTypeJob.value;
+    popup.classList.add('popup__hidden');
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit); 
+profileEditButton.addEventListener('click', function () {
+    popup.classList.remove('popup__hidden');
+    console.log(popup);
+    popupInputTypeName.value = profileTitle.textContent;
+    popupInputTypeJob.value = profileText.textContent;
+});
 
+popupButtonClose.addEventListener('click', function () {
+    popup.classList.add('popup__hidden');
+});
+
+popupForm.addEventListener('submit', formSave);
