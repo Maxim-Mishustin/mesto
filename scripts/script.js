932624aecf03550216c8f4c3ef4158cbfde8e175
Maxim Mishustin <vs1.136@yandex.ru>
2023-02-19 20:48:57 +0300

    отправить на ревью 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,36 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupButtonClose = document.querySelector('.popup__button-close');
+// console.log(popupButtonClose);
 const profileTitle = document.querySelector('.profile__title');
+// console.log(profileTitle.textContent);
 const popupInputTypeName = document.querySelector('.popup__input_type_name');
+// console.log(popupInputTypeName.value);
 const profileText = document.querySelector('.profile__text');
+// console.log(profileText.textContent);
 const popupInputTypeJob = document.querySelector('.popup__input_type_job');
+// console.log(popupInputTypeJob.value);
 const popupForm = document.querySelector('.popup__form');
+// console.log(popupForm);
 
-function formSave (evt) {
-    evt.preventDefault(); 
+function handleFormSubmit (evt) {
+    evt.preventDefault();
     profileTitle.textContent = popupInputTypeName.value;
     profileText.textContent = popupInputTypeJob.value;
-    popup.classList.add('popup_hidden');
+    closePopup();
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
 }
 
 profileEditButton.addEventListener('click', function () {
-    popup.classList.remove('popup_hidden');
-    console.log(popup);
+    popup.classList.add('popup_opened');
+    // console.log(popup);
     popupInputTypeName.value = profileTitle.textContent;
     popupInputTypeJob.value = profileText.textContent;
 });
 
-popupButtonClose.addEventListener('click', function () {
-    popup.classList.add('popup_hidden');
-});
+popupButtonClose.addEventListener('click', closePopup);
 
-popupForm.addEventListener('submit', formSave);
+popupForm.addEventListener('submit', handleFormSubmit);
