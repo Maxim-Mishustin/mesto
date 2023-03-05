3f932e16db73b2eb7cb047ae11b345c0904b25e4
Maxim Mishustin <vs1.136@yandex.ru>
2023-03-05 18:37:13 +0300

    перед ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,163 +1,130 @@
-// создание переменной для кнопки редактирования
 const profileEditButton = document.querySelector('.profile__edit-button');
-// создали переменную попап
-const popup = document.querySelector('.popup');
-// создали переменную закрытия попапа
-const popupButtonClose = document.querySelector('.popup__button-close');
-console.log(popupButtonClose);
-// создали переменную для профайл тайтл
-const profileTitle = document.querySelector('.profile__title');
-console.log(profileTitle.textContent);
-// создали переменную для попап нейм в попапе
-const popupInputTypeName = document.querySelector('.popup__input_type_name');
-console.log(popupInputTypeName.value);
-// создали переменную для профайл текст
-const profileText = document.querySelector('.profile__text');
-console.log(profileText.textContent);
-// создали переменную для попап профессии
-const popupInputTypeJob = document.querySelector('.popup__input_type_job');
-console.log(popupInputTypeJob.value);
-// создали переменную для профайл нейм в попапе
-const popupForm = document.querySelector('.popup__form');
-console.log(popupForm);
-
-// функция сохранить (дали готовую в ТЗ)
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-    profileTitle.textContent = popupInputTypeName.value;
-    profileText.textContent = popupInputTypeJob.value;
-    closePopup();
-}
-
-// функция закрытия попапа
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
-
-// добавили слушатель клика
-profileEditButton.addEventListener('click', function () {
-    popup.classList.add('popup_opened');
-    console.log(popup);
-    popupInputTypeName.value = profileTitle.textContent;
-    popupInputTypeJob.value = profileText.textContent;
-});
-
-// добавили слушатель для кнопки закрытия попапа
-popupButtonClose.addEventListener('click', closePopup);
-
-// добавили слушатель для кнопки сохранить
-popupForm.addEventListener('submit', handleFormSubmit);
-
-
-
-// -------------------------------------------------------
-
-
-
-
-
+const popupTypeEdit = document.querySelector('.popup_type_edit');
+const popupButtonCloseEdit = document.querySelector('.popup__button-close-edit');
 // создание переменной для кнопки добавления карточки Add
 const profileAddButton = document.querySelector('.profile__add-button');
-
-
-
-
-// массив карточек
-const cards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    alt: 'Архыз'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    alt: 'Челябинская область'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-    alt: 'Иваново'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    alt: 'Камчатка'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-    alt: 'Холмогорский район'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-    alt: 'Байкал'
-  }
-];
-
-
-
+const popupTypeAdd = document.querySelector('.popup_type_add');
+const popupFormAdd = document.querySelector('.popup__form-add');
+const popupButtonCloseAdd = document.querySelector('.popup__button-close-add');
+// записали в переменную поле для названия нового места
+const popupInputTypeNewPlace = document.querySelector('.popup__input_type_new-place');
+// записали в переменную поле для ссылки
+const popupInputTypeUrl = document.querySelector('.popup__input_type_url');
+// всё что касается биг кард и его попапа
+const popupTypeBigCard = document.querySelector('.popup_type_big-card');
+const popupButtonCloseBigCard = document.querySelector('.popup__button-close-big-card');
+const popupCardImage = document.querySelector('.popup__card-image');
+const popupCardTitle = document.querySelector('.popup__card-title');
+const profileTitle = document.querySelector('.profile__title');
+const popupInputTypeName = document.querySelector('.popup__input_type_name');
+const profileText = document.querySelector('.profile__text');
+const popupInputTypeJob = document.querySelector('.popup__input_type_job');
+const popupForm = document.querySelector('.popup__form');
 // записали в переменную весь элементс
 const elementGallery = document.querySelector('.elements');
-console.log(elementGallery);
-
 // записали в переменную по айди весь темплейт с контентом
 const templateCards = document.querySelector('#templateCards').content;
-console.log(templateCards);
 
+// функция submit сохранить 
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = popupInputTypeName.value;
+  profileText.textContent = popupInputTypeJob.value;
+  closePopup(popupTypeEdit);
+};
+
+// функция закрытия попапа общая
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+};
+
+// функция создания карточки
 const createCard = card => {
-  // записали в newElement весь темплейт по айди
+  // записали в newElement темплейт с содержимым по айди
   const newElement = templateCards.cloneNode(true);
-  console.log(newElement);
 
   // назначили переменную с заголовком темплейта
   const elementTitle = newElement.querySelector('.element__title');
-  console.log(elementTitle);
-
   // записали в переменную класс фото из темплейта
   const elementImage = newElement.querySelector('.element__image');
-  console.log(elementImage);
-
   // записали в elementTitle текст из массива
   elementTitle.textContent = card.name;
-  console.log(elementTitle);
-
   // задаём значение в elementImage фото по сслыке из массива
   elementImage.setAttribute('src', card.link);
-  console.log(elementImage);
+  elementImage.setAttribute('alt', card.alt);
 
+  elementImage.addEventListener('click', function () {
+    popupTypeBigCard.classList.add('popup_opened');
+    popupCardImage.setAttribute('src', elementImage.src);
+    popupCardTitle.textContent = card.name;
+  });
+
+  const elementLike = newElement.querySelector('.element__like');
+  elementLike.addEventListener('click', function(evt) {
+    evt.target.classList.add('element__like_active');
+  });
 
   // добавление функции удаления карточки
   const deleteButton = newElement.querySelector('.element__delete');
-  deleteButton.addEventListener('click', handleDeleteButtonClick)
+  deleteButton.addEventListener('click', handleDeleteButtonClick);
 
-  elementGallery.prepend(newElement); // вынести в новую функцию
-  
+  return newElement;
 };
 
-cards.forEach(createCard);
+// функция добавления карточки из формы
+const addCardFormSubmit = evt => {
+  evt.preventDefault();
+  const newCard = new Object();
+  newCard.name = popupInputTypeNewPlace.value;
+  newCard.link = popupInputTypeUrl.value;
+  addCard(newCard);
+  closePopup(popupTypeAdd);
+};
 
 // функция удаления карточки вынесенная отдельно
 function handleDeleteButtonClick(event) {
   const button = event.target
   const element = button.closest('.element')
   element.remove()
-}
-
-
-
-
-// добавление лайка
-const elementLike = elementsTemplate.querySelector('.element__like');
-console.log(elementLike);
-
-function like() {
-  elementLike.classList.toggle('.element__like_active');
-  console.log(click);
-
-  elementLike.addEventListener('click', like);
 };
 
+// функция добавления карточки в dom
+const addCard = card => {
+  elementGallery.prepend(createCard(card));
+};
 
+// добавили слушатель клика для Edit
+profileEditButton.addEventListener('click', function () {
+  popupTypeEdit.classList.add('popup_opened');
+  popupInputTypeName.value = profileTitle.textContent;
+  popupInputTypeJob.value = profileText.textContent;
+});
 
+// добавили слушатель для кнопки закрытия Edit
+popupButtonCloseEdit.addEventListener('click', function () {
+  closePopup(popupTypeEdit);
+});
+
+// добавили слушатель для кнопки закрытия Add
+popupButtonCloseAdd.addEventListener('click', function () {
+  closePopup(popupTypeAdd);
+});
+
+// добавили слушатель для кнопки закрытия Big Card
+popupButtonCloseBigCard.addEventListener('click', function () {
+  closePopup(popupTypeBigCard);
+});
+
+// добавили слушатель для кнопки сохранить
+popupForm.addEventListener('submit', handleFormSubmit);
+
+// добавили слушатель клика для закрытия PopupAdd
+profileAddButton.addEventListener('click', function () {
+  popupTypeAdd.classList.add('popup_opened');
+});
+
+// вызов функции addcard на каждый элемент массива
+cards.forEach(addCard);
+
+// вызов функции для кнопки создать
+popupFormAdd.addEventListener('submit', addCardFormSubmit);
